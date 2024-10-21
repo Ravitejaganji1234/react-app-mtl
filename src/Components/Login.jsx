a1547f0fde5918306fef1173571722eb6b0093f5
MTL1017 <ravitejaganji49@gmail.com>
2024-10-21 17:33:01 +0530

    changed anchor tag to link

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import '../SharedCSS/SharedCss.css';
 import { useNavigate, Link } from 'react-router-dom';
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import '../SharedCSS/SharedCss.css';
 import axios from "axios";
 import Loader from "../Assets/Loader"; // Ensure Loader is correctly imported
