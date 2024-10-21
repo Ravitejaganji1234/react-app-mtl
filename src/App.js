c7dc0d921f511c3c47a9c26369392a167b1ccc3d
Raviteja <MTL1017@middlewaretalents.in>
2024-10-21 09:26:22 +0530

    Initial commit for React app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route, Navigate, useLocation} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Navbar from './HomePage/Navbar';
 import Login from './Components/Login';
 import Register from './Components/Register';
 import Dashboard from './HomePage/Dashboard';
 import Employee from './HomePage/Employee';
+import EmployeeDetails from "./EmployeeComponents/EmployeeDetails";
 
 function App() {
-    // Check if email is present in localStorage
-    const isLoggedIn = localStorage.getItem('email');
+    // Check if email and role are present in localStorage
+    const isLoggedIn = localStorage.getItem('email') && localStorage.getItem('role');
 
     return (
         <BrowserRouter>
@@ -18,6 +19,8 @@ function App() {
                 {/* Redirect to login if not logged in */}
                 <Route path="/" element={isLoggedIn ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
                 <Route path="/*" element={<Main />} />
+                {/* Catch-all route for unmatched paths */}
+                <Route path="*" element={<Navigate to={isLoggedIn ? "/dashboard" : "/login"} />} />
             </Routes>
         </BrowserRouter>
     );
@@ -34,6 +37,7 @@ function Main() {
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/employee" element={<Employee />} />
                 {/* Add more routes as needed */}
+                <Route path="/employeedetails/:employeeId" element={<EmployeeDetails />} />
             </Routes>
         </>
     );
