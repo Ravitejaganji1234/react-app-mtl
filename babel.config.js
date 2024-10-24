34d8cd3357b718ace556e6fcc0896c7af3a29da8
MTL1017 <ravitejaganji49@gmail.com>
2024-10-24 09:48:18 +0530

    New changes

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = {
     presets: [
         '@babel/preset-env',  // For handling ES6+ syntax
+         {
+                targets: {
+                  node: 'current',  // Ensure proper Node.js version handling
+                },
+                modules: false  // Let Webpack handle modules; this is important for ES modules
+              }
+            ],
         '@babel/preset-react'  // For handling React JSX
     ]
 };
